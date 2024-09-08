22635a36c69214118f1f60cb6625424707dc0074
Pathum Jeewantha Dissanayake <127170659+PJDissanayake@users.noreply.github.com>
2024-09-08 23:26:24 +0530

    Update EEPROM_WL.C

--- src/EEPROM_WL.C
+++ src/EEPROM_WL.C
@@ -12,25 +12,28 @@
 #include "EEPROM_SPI.h"
 #include "EEPROM_Data_struct.h"
 
+uint8_t mem;
+uint8_t pag;
+uint32_t base_adr;
 
 uint8_t EEPROM_init(uint8_t mem_size,uint8_t page_size,uint8_t no_of_dat){ //input mem size Kilo bytes value and Page size bytes value
-	uint8_t mem=mem_size*1024;
-	uint8_t pag=page_size;
-	uint32_t base_adr=mem/no_of_dat;
+	mem=mem_size*1024;
+	pag=page_size;
+	base_adr=mem/no_of_dat;
 }
 
 void CALL_EEPROM(uint8_t data_no,uint8_t data_size,bool command){ //command 1 for write; 0 for read  ***input  byte value of data size
       uint32_t u_bound=base_adr*(data_no);
       uint32_t l_bound=u_bound-base_adr;
 
-      SEN_check(l_bound, u_bound, data_size,last_writeadd,currunt_page);
-      uint32_t page_bound = l_bound + (currunt_page * Pag) + Pag;
+      SEN_check(l_bound, u_bound, data_size,last_writeadd,current_page);
+      uint32_t page_bound = l_bound + (current_page * Pag + Pag);
 
       if(command){//write
 	  	if(last_writeadd==0){
 			next_write_add=0;
 		}
-		else(){
+		else{
       		next_write_add=(last_writeadd+(data_size+1));
 		}
 		
@@ -53,23 +56,23 @@ void CALL_EEPROM(uint8_t data_no,uint8_t data_size,bool command){ //command 1 fo
   }
 
 
-uint32_t SEN_check(uint32_t start,uint32_t stop,uint8_t step,uint32_t last_add,uint32_t curru_page){
-  	for(uint32_t c=0x01;c<(base_adr/pag));c++){
+uint32_t SEN_check(uint32_t start,uint32_t stop,uint8_t step,uint32_t last_add,uint32_t curre_page){
+  	for(uint32_t c=0x01;c<(base_adr/pag);c++){
   	for (uint32_t var = (start+((stop-start)/pag)*c); (var+step+1)<stop; var+=(step+1)) {
   		read_1byte(var);
   		if(rec_data1[0]==sen_byte){
   			last_add=var;
-  			curru_page=c;
+  			curre_page=c;
   			break;
   			}
   		else if(var>=(stop-(step+1))){
 			if(data_no==1){
 				last_add=0;
-  				curru_page=0;	
+  				curre_page=0;	
 			}
-			else(){
+			else{
   				last_add=start-(step+1);
-  				curru_page=0;
+  				curre_page=0;
 			}
   			break;
   			}
