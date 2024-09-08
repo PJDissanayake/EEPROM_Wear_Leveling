3e3db27c9bebe70ba0e936c7d49993e7ace245f8
Pathum Jeewantha Dissanayake <127170659+PJDissanayake@users.noreply.github.com>
2024-09-09 00:07:18 +0530

    Update EEPROM_WL.C

--- src/EEPROM_WL.C
+++ src/EEPROM_WL.C
@@ -15,6 +15,9 @@
 uint8_t mem;
 uint8_t pag;
 uint32_t base_adr;
+uint8_t data_no;
+uint8_t data_size;
+bool command;
 
 uint8_t EEPROM_init(uint8_t mem_size,uint8_t page_size,uint8_t no_of_dat){ //input mem size Kilo bytes value and Page size bytes value
 	mem=mem_size*1024;
@@ -23,12 +26,12 @@ uint8_t EEPROM_init(uint8_t mem_size,uint8_t page_size,uint8_t no_of_dat){ //inp
 	return 0;  // Return success or status code
 }
 
-void CALL_EEPROM(uint8_t data_no,uint8_t data_size,bool command){ //command 1 for write; 0 for read  ***input  byte value of data size
+void CALL_EEPROM(data_no, data_size, command){ //command 1 for write; 0 for read  ***input  byte value of data size
       uint32_t u_bound=base_adr*(data_no);
       uint32_t l_bound=u_bound-base_adr;
 
-      SEN_check(l_bound, u_bound, data_size,last_writeadd,current_page);
-      uint32_t page_bound = l_bound + (current_page * Pag + Pag);
+      SEN_check(l_bound, u_bound, data_size, &last_writeadd, &current_page);
+      uint32_t page_bound = l_bound + (current_page * pag + pag);
 
       if(command){//write
 	  	if(last_writeadd==0){
@@ -57,30 +60,30 @@ void CALL_EEPROM(uint8_t data_no,uint8_t data_size,bool command){ //command 1 fo
   }
 
 
-uint32_t SEN_check(uint32_t start,uint32_t stop,uint8_t step,uint32_t last_add,uint32_t curre_page){
+uint32_t SEN_check(uint32_t start, uint32_t stop, uint8_t step, uint32_t *last_add, uint32_t *curre_page){
   	for(uint32_t c=0x01;c<(base_adr/pag);c++){
   	for (uint32_t var = (start+((stop-start)/pag)*c); (var+step+1)<stop; var+=(step+1)) {
   		read_1byte(var);
   		if(rec_data1[0]==sen_byte){
-  			last_add=var;
-  			curre_page=c;
+  			*last_add=var;
+  			*current_page=c;
   			break;
   			}
   		else if(var>=(stop-(step+1))){
 			if(data_no==1){
-				last_add=0;
-  				curre_page=0;	
+				*last_add=0;
+  				*current_page=0;	
 			}
 			else{
-  				last_add=start-(step+1);
-  				curre_page=0;
+  				*last_add=start-(step+1);
+  				*current_page=0;
 			}
   			break;
   			}
   		}
   	 }
 
-	return last_add;  // Return the updated last_add value
+	return *last_add;  
   	}
 
 
